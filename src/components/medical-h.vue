72b7ca0554f96d76caa302d40ad43c56fff094c8
RodgersX <brianmawira2@gmail.com>
2020-07-04 22:35:27 +0300

    accordion achieved

--- src/components/medical-h.vue
+++ src/components/medical-h.vue
@@ -4,46 +4,46 @@
             <div class="tab-title d-flex justify-space-between align-center">
                 <h4>Medical History</h4>
                 <div>
-                <input v-if="search" type="text" class="pa-1 grey lighten-3" style="width: 20rem;">
+                <input v-if="search" type="text" class="seacrh-input pa-1 grey lighten-3" style="width: 20rem;">
                 <v-btn icon @click="search = true">
                     <i class="bx bx-search bx-sm"></i>
                 </v-btn>
                 </div>
             </div>
             <v-timeline dense>
-                <v-timeline-item 
-                fill-dot 
+                <v-timeline-item
+                fill-dot
                 small 
                 color="red"
-                v-for="hs in history"
-                :key="hs.id">
-                    <div class="panel-group" id="accordion">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-                                    <v-list>
-                                        <v-list-item>
-                                        <v-list-item-content class="text-center">
-                                            <p>{{ hs.date }}</p>
-                                            <p>{{ hs.month }}</p>
-                                        </v-list-item-content>
-                                        <v-list-item-content class="text-center">
-                                            <p>{{ hs.title }}</p>
-                                            <p class="red--text">{{ hs.dr }}</p>
-                                        </v-list-item-content>
-                                        <v-list-item-content class="text-center">
-                                            <p>{{ hs.time }}</p>
-                                        </v-list-item-content>
-                                        </v-list-item>
-                                        <v-divider></v-divider>
-                                    </v-list>
-                                </a>
+                v-for="(hs, i) in history"
+                :key="i">
+                    <div id="accordion">
+                        <div 
+                        class="d-flex justify-space-between align-center" 
+                        data-toggle="collapse"
+                        :data-target="'#' + hs.id"
+                        :aria-controls="hs.id" 
+                        :id="hs.target">
+                            <p>{{ hs.date }} <br> {{ hs.month }}</p>
+                            <p class="ml-3">{{ hs.title }} <span class="red--text">{{ hs.dr }}</span></p>
+                            <p>11.30PM</p>
+                        </div>
+                        <div :id="hs.id" class="collapse" :aria-labelledby="hs.target" data-parent="#accordion">
+                            <div class="card-header">
+                                <h5>Your symptoms</h5>
+                                <p>{{ hs.symptoms }}</p>
+                                <h5>Doctors Note</h5>
+                                <p>{{ hs.note1 }}</p>
+                                <p>{{ hs.note2 }}</p>
+                                <h5>Next visit</h5>
+                                <div class="d-flex">
+                                    <v-chip dark>{{ hs.nextDate }}</v-chip>
+                                    <v-btn text color="red" class="btn ml-3">Re-schedule</v-btn>
+                                </div>
                             </div>
-                            <div id="collapse1" class="panel-collapse collapse in">
-                                <div class="panel-body">{{ hs.details }}</div>
-                            </div>
-                        </div>           
+                        </div>  
                     </div>
+                    <v-divider></v-divider>
                 </v-timeline-item>
             </v-timeline>
         </v-sheet>
@@ -54,13 +54,15 @@
 export default {
     data() {
         return {
+            more: false,
+            search: false,
             history: [
-                {date: '8', month: 'Apr', title: 'Removal Stitches', time: '11.00PM', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum est odit minus! Qui sunt repudiandae iure praesentium impedit tempora!'},
-                {date: '5', month: 'Apr', title: 'Cholecystectomy Surgery', dr: 'Dr. Paiki', time: '07.00PM', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum est odit minus! Qui sunt repudiandae iure praesentium impedit tempora!'},
-                {date: '4', month: 'Apr', title: 'Gall Stones Diagnosis', time: '16.20PM', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum est odit minus! Qui sunt repudiandae iure praesentium impedit tempora!'},
-                {date: '13', month: 'Mar', title: 'General Physical check-up', dr: 'Dr. Mwangi', time: '11.00PM', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum est odit minus! Qui sunt repudiandae iure praesentium impedit tempora!'},
-                {date: '13', month: 'Feb', title: 'Influenza', time: '15.03PM', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum est odit minus! Qui sunt repudiandae iure praesentium impedit tempora!'},
-                {date: '4', month: 'Jan', title: 'Health Check up', time: '15.20PM', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatum est odit minus! Qui sunt repudiandae iure praesentium impedit tempora!'}
+                {id: 'collapseOne', target: 'headingOne', date: '8', month: 'Apr', title: 'Removal Stitches', time: '11.00PM', symptoms: 'Seeing difficulty', note1: 'Required blood do', note2: 'Bile leak', nextDate: '03-04-2020'},
+                {id: 'collapseTwo', target: 'headingTwo', date: '5', month: 'Apr', title: 'Cholecystectomy Surgery', dr: 'Dr. Paiki', time: '07.00PM', symptoms: 'Seeing difficulty', note2: 'Bile leak', nextDate: '03-04-2020'},
+                {id: 'collapseThree', target: 'headingThree', date: '4', month: 'Apr', title: 'Gall Stones Diagnosis', time: '16.20PM', symptoms: 'Seeing difficulty', note2: 'Bile leak', nextDate: '03-04-2020'},
+                {id: 'collapseFour', target: 'headingFour', date: '13', month: 'Mar', title: 'General Physical check-up', dr: 'Dr. Mwangi', time: '11.00PM', symptoms: 'Seeing difficulty', note1: 'Required blood donation', note2: 'Bile leak', nextDate: '03-04-2020'},
+                {id: 'collapseFive', target: 'headingFive', date: '13', month: 'Feb', title: 'Influenza', time: '15.03PM', symptoms: 'Seeing difficulty', note1: 'Required blood donation', nextDate: '03-04-2020'},
+                {id: 'collapseSix', target: 'headingSix', date: '4', month: 'Jan', title: 'Health Check up', time: '15.20PM', symptoms: 'Seeing difficulty', note1: 'Required blood donation', nextDate: '03-04-2020'}
             ]
         }
     }
@@ -69,8 +71,19 @@ export default {
 
 <style scoped>
 
-.panel-heading a:hover {
+.panel-heading:hover {
     text-decoration: none;
+    cursor: pointer;
+}
+
+.seacrh-input {
+    border-radius: 5px;
+    margin-right: 30px;
+    background-color: #efefef;
+}
+
+.btn {
+    text-transform: capitalize;
 }
 
 </style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,40 +1,45 @@
 <template>
     <div class="test">
-        <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-                            <v-list>
-                                <v-list-item>
-                                <v-list-item-content class="text-center">
-                                    <p>8</p>
-                                    <p>Apr</p>
-                                </v-list-item-content>
-                                <v-list-item-content class="text-center">
-                                    <p>Endoscopy</p>
-                                    <p class="red--text">Dr. Kiarie</p>
-                                </v-list-item-content>
-                                <v-list-item-content class="text-center">
-                                    <P>11.30PM</P>
-                                </v-list-item-content>
-                                </v-list-item>
-                                <v-divider></v-divider>
-                            </v-list>
-                        </a>
+        <div id="accordion">
+            <div class="card">
+                <div class="card-header" data-toggle="collapse" data-target="#collapseOne" aria-controls="collapseOne" id="headingOne">
+                    <h5 class="mb-0">
+                        Collapsible Group Item #1
+                    </h5>
                 </div>
-                <div id="collapse1" class="panel-collapse collapse in">
-                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.</div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
                 </div>
-            </div>           
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <h5 class="mb-0">
+                        Collapsible Group Item #2
+                    </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div class="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-.panel-heading a:hover {
-    text-decoration: none;
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
 }
+</script>
+
+<style scoped>
+
 </style>
